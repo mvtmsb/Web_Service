ddaacf9a818117de50da1d53ef3e1d282f29319e
mvtmsb <65546831+mvtmsb@users.noreply.github.com>
2020-05-18 16:09:41 +0300

    Add files via upload

---
+++ SharedComponents/Attractions.AttractionListQuery/ApplicationServices/GetAttractionListUseCase/GetAttractionListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using Attractions.DomainObjects;
+using Attractions.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attractions.ApplicationServices.GetAttractionListUseCase
+{
+    public class GetAttractionListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<Attraction> Attractions { get; }
+
+        public GetAttractionListUseCaseResponse(IEnumerable<Attraction> attractions) => Attractions = attractions;
+    }
+}
---
+++ SharedComponents/Attractions.BusinessLogic/DomainObjects/Ports/IAttractionRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Attractions.DomainObjects.Ports
+{
+    public interface IReadOnlyAttractionRepository
+    {
+        Task<Attraction> GetAttraction(long id);
+
+        Task<IEnumerable<Attraction>> GetAllAttractions();
+
+        Task<IEnumerable<Attraction>> QueryAttractions(ICriteria<Attraction> criteria);
+
+    }
+
+    public interface IAttractionRepository
+    {
+        Task AddRoute(Attraction attraction);
+
+        Task RemoveRoute(Attraction attraction);
+
+        Task UpdateRoute(Attraction attraction);
+    }
+}
