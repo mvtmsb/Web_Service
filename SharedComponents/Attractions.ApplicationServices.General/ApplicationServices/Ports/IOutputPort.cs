ac92a32ec2488813ccf25e257878376d38b41672
mvtmsb <65546831+mvtmsb@users.noreply.github.com>
2020-05-18 16:13:21 +0300

    Add files via upload

---
+++ SharedComponents/Attractions.ApplicationServices.General/ApplicationServices/Ports/IOutputPort.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attractions.ApplicationServices.Ports
+{
+    public interface IOutputPort<in TUseCaseResponse>
+    {
+        void Handle(TUseCaseResponse response);
+    }
+}
---
+++ SharedComponents/Attractions.AttractionListQuery/ApplicationServices/GetAttractionListUseCase/GetAttractionListUseCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using Attractions.DomainObjects;
+using Attractions.DomainObjects.Ports;
+using Attractions.ApplicationServices.Ports;
+
+namespace Attractions.ApplicationServices.GetAttractionListUseCase
+{
+    public class GetAttractionListUseCase : IGetAttractionListUseCase
+    {
+        private readonly IReadOnlyAttractionRepository _readOnlyAttractionRepository;
+
+        public GetAttractionListUseCase(IReadOnlyAttractionRepository readOnlyAttractionRepository) 
+            => _readOnlyAttractionRepository = readOnlyAttractionRepository;
+
+        public async Task<bool> Handle(GetAttractionListUseCaseRequest request, IOutputPort<GetAttractionListUseCaseResponse> outputPort)
+        {
+            IEnumerable<Attraction> attractions = null;
+            if (request.AttractionId != null)
+            {
+                var attraction = await _readOnlyAttractionRepository.GetAttraction(request.AttractionId.Value);
+                attractions = (attraction != null) ? new List<Attraction>() { attraction } : new List<Attraction>();
+                
+            }
+            else if (request.Location != null)
+            {
+                attractions = await _readOnlyAttractionRepository.QueryAttractions(new LocationCriteria(request.Location));
+            }
+            else
+            {
+                attractions = await _readOnlyAttractionRepository.GetAllAttractions();
+            }
+            outputPort.Handle(new GetAttractionListUseCaseResponse(attractions));
+            return true;
+        }
+    }
+}
