366bf4274f1ba6b1f9f3b352639ead5f62daf888
mvtmsb <65546831+mvtmsb@users.noreply.github.com>
2020-05-18 16:20:46 +0300

    Add files via upload

---
+++ Attractions.WebService/ApplicationServices/GetAttractionListUseCase/LocationCriteria.cs
@@ -0,0 +1,20 @@
+﻿using Attractions.DomainObjects;
+using Attractions.DomainObjects.Ports;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Attractions.ApplicationServices.GetAttractionListUseCase
+{
+    public class LocationCriteria : ICriteria<Attraction>
+    {
+        public string Location { get; }
+
+        public LocationCriteria(string location)
+            => Location = location;
+
+        public Expression<Func<Attraction, bool>> Filter
+            => (at => at.Location == Location);
+    }
+}
---
+++ Attractions.WebService/InfrastructureServices/Controllers/AttractionsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Attractions.DomainObjects;
+using Attractions.ApplicationServices.GetAttractionListUseCase;
+using Attractions.InfrastructureServices.Presenters;
+
+namespace Attractions.InfrastructureServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AttractionsController : ControllerBase
+    {
+        private readonly ILogger<AttractionsController> _logger;
+        private readonly IGetAttractionListUseCase _getAttractionListUseCase;
+
+        public AttractionsController(ILogger<AttractionsController> logger,
+                                IGetAttractionListUseCase getAttractionListUseCase)
+        {
+            _logger = logger;
+            _getAttractionListUseCase = getAttractionListUseCase;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllAttractions()
+        {
+            var presenter = new AttractionListPresenter();
+            await _getAttractionListUseCase.Handle(GetAttractionListUseCaseRequest.CreateAllAttractionsRequest(), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("{attractionId}")]
+        public async Task<ActionResult> GetAttraction(long attractionId)
+        {
+            var presenter = new AttractionListPresenter();
+            await _getAttractionListUseCase.Handle(GetAttractionListUseCaseRequest.CreateAttractionRequest(attractionId), presenter);
+            return presenter.ContentResult;
+        }
+
+        [HttpGet("Locations/{location}")]
+        public async Task<ActionResult> GetLocationFilter(string location)
+        {
+            var presenter = new AttractionListPresenter();
+            await _getAttractionListUseCase.Handle(GetAttractionListUseCaseRequest.CreateLocationAttractionsRequest(location), presenter);
+            return presenter.ContentResult;
+        }
+    }
+}
