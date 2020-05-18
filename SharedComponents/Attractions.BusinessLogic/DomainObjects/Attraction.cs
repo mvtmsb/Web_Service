c56aacb248b9e16268b76408eec63b253178a1e6
mvtmsb <65546831+mvtmsb@users.noreply.github.com>
2020-05-18 16:07:40 +0300

    Add files via upload

---
+++ SharedComponents/Attractions.BusinessLogic/DomainObjects/Attraction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attractions.DomainObjects
+{
+  public class Attraction : DomainObject
+    {
+    
+        public string NameObject { get; set; }
+
+        public string Location { get; set; }
+        public string Admission { get; set; }
+        public string PeriodA { get; set; }
+
+    }
+}
